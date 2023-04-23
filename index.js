1dfb8671b74ccad790a845d78ec8b3ca618afa08
vamshi <alugojuvamshi@gmail.com>
2023-04-23 19:15:22 +0530

    connected to localhost

--- index.js
+++ index.js
@@ -7,60 +7,90 @@ let options = document.querySelector("#options");
 let submit_btn = document.querySelector("#submit-btn")
 submit_btn.addEventListener("click" , addItems);
 
-let deletebtn = document.querySelector("#deletebtn");
-deletebtn.addEventListener("click" , delete_item)
 
-let editbtn = document.querySelector("#editbtn");
-editbtn.addEventListener("click" , edit_item)
+window.addEventListener("load",LoadItems);
 
-
+function LoadItems()
+{
+    //request to be sent
+    axios.get("http://localhost:3000")
+    .then(res=>{
+       res.data.forEach(item=>{
+        addTodom(item);
+       })
+    })
+    .catch(err=>console.log(err));
+}
 
 
  function addItems(e)
 {  
     e.preventDefault();
-    
-  let newli =   document.createElement("li");
-  let deletebtn = document.createElement("button");
-  let editbtn = document.createElement("button");
-
-  deletebtn.classList = "btn btn-danger btn-sm";
-  editbtn.classList = "btn btn-primary btn-sm";
-  newli.classList = " list-item list-group-item"
-
-  deletebtn.id = "deletebtn"
-  editbtn.id = "editbtn"
-  deletebtn.addEventListener("click" , delete_item)
-  editbtn.addEventListener("click" , edit_item)
-
-
-  deletebtn.innerText = "delete"
-  editbtn.innerText = "edit";
-  newli.innerText =  amount.value + "-" + description.value+"-"+options.options[options.value].innerText
-  
-  newli.appendChild(deletebtn);
-  newli.appendChild(editbtn)
-  ul_items.appendChild(newli)
- 
+    console.log("helo");
+    const Expense = {
+        amount : amount.value,
+        description:description.value,
+        type:options.options[options.value].innerText
+    }
+    axios.post("http://localhost:3000",Expense)
+    .then(res=>{
+         addTodom(res.data);
+    })
+    .catch(err=>console.log(err));
 }
 
+function addTodom(item)
+{
+  
+    let newli =   document.createElement("li");
+    let deletebtn = document.createElement("button");
+    let editbtn = document.createElement("button");
+  
+    deletebtn.classList = "btn btn-danger btn-sm";
+    editbtn.classList = "btn btn-primary btn-sm";
+    newli.classList = " list-item list-group-item"
+    
+    newli.id = item.id;
+    deletebtn.id = "deletebtn"
+    editbtn.id = "editbtn"
+    deletebtn.addEventListener("click" , delete_item)
+    editbtn.addEventListener("click" , edit_item)
+  
+  
+    deletebtn.innerText = "delete"
+    editbtn.innerText = "edit";
+    newli.innerText =  item.Amount + "-" + item.Description+"-"+item.Type
+    
+    newli.appendChild(deletebtn);
+    newli.appendChild(editbtn)
+    ul_items.appendChild(newli)
+}
 
 function delete_item(e)
 {
     
-    let li = e.target.parentElement;
-    console.log(li)
-    ul_items.removeChild(li)
+    let li = e.target.parentElement
+    let id = li.getAttribute("id");
+     
+    axios.delete(`http://localhost:3000/${id}`)
+    .then(res=>{
+        ul_items.removeChild(li)
+    })
+    .catch(err=>console.log(err));
 }
 
 function edit_item(e)
 {   
 
     let li = e.target.parentElement;
+    let id = li.getAttribute("id");
     let words = li.innerText.split("-");
     amount.value = words[0];
     description.value = words[1];
     console.log(words)
-    ul_items.removeChild(li)
-
+    axios.delete(`http://localhost:3000/${id}`)
+    .then(res=>{
+        ul_items.removeChild(li)
+    })
+    .catch(err=>console.log(err));
 }
